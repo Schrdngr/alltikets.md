2fa9d10cc5c223e0bd9382d921fbf93e8c3a79d0
Schrdngr <73908443+Schrdngr@users.noreply.github.com>
2025-06-26 14:15:26 +0300

    Update schemas.py
    Code added

--- backend/schemas.py
+++ backend/schemas.py
@@ -1,1 +1,16 @@
+from pydantic import BaseModel
+
+class Event(BaseModel):
+    id: int
+    title: str
+    description: str | None = None
+    date: str | None = None
+    location: str | None = None
+    price: str | None = None
+    url: str | None = None
+    image_url: str | None = None
+    source: str | None = None
+
+    class Config:
+        orm_mode = True
 
