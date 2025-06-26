3f14f6fa632010318a69cba3ab3cf2eaa9e2dd65
Schrdngr <73908443+Schrdngr@users.noreply.github.com>
2025-06-26 14:15:47 +0300

    Update models.py
    code added

--- backend/models.py
+++ backend/models.py
@@ -1,1 +1,17 @@
+import sqlalchemy
+from backend.database import metadata
+
+events = sqlalchemy.Table(
+    "events",
+    metadata,
+    sqlalchemy.Column("id", sqlalchemy.Integer, primary_key=True),
+    sqlalchemy.Column("title", sqlalchemy.String, nullable=False),
+    sqlalchemy.Column("description", sqlalchemy.Text),
+    sqlalchemy.Column("date", sqlalchemy.String), # Можно заменить на DateTime
+    sqlalchemy.Column("location", sqlalchemy.String),
+    sqlalchemy.Column("price", sqlalchemy.String),
+    sqlalchemy.Column("url", sqlalchemy.String),
+    sqlalchemy.Column("image_url", sqlalchemy.String),
+    sqlalchemy.Column("source", sqlalchemy.String), # iticket.md, mticket.md и т.д.
+)
 
