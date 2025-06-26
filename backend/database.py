aa1c60b20679c6bdfe0fc3d9ee3e4a9de96ea04d
Schrdngr <73908443+Schrdngr@users.noreply.github.com>
2025-06-26 14:15:06 +0300

    Update database.py
    code added

--- backend/database.py
+++ backend/database.py
@@ -1,1 +1,12 @@
+import os
+import databases
+import sqlalchemy
+
+# Загружаем DATABASE_URL из переменной окружения или .env
+DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://user:pass@localhost:5432/events_db")
+
+# База данных и движок
+database = databases.Database(DATABASE_URL)
+metadata = sqlalchemy.MetaData()
+engine = sqlalchemy.create_engine(DATABASE_URL)
 
