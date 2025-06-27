fa8612aec5849e947aba8d631e5799ffc3d6208f
Schrdngr <73908443+Schrdngr@users.noreply.github.com>
2025-06-27 08:06:36 +0300

    Update i18n.js

--- frontend/src/i18n.js
+++ frontend/src/i18n.js
@@ -1,1 +1,37 @@
+import i18n from 'i18next'
+import { initReactI18next } from 'react-i18next'
+
+const resources = {
+  en: {
+    translation: {
+      upcomingEvents: 'Upcoming Events',
+      language: 'Language',
+    }
+  },
+  ru: {
+    translation: {
+      upcomingEvents: 'Предстоящие события',
+      language: 'Язык',
+    }
+  },
+  ro: {
+    translation: {
+      upcomingEvents: 'Evenimente viitoare',
+      language: 'Limba',
+    }
+  }
+}
+
+i18n
+  .use(initReactI18next)
+  .init({
+    resources,
+    lng: 'en',
+    fallbackLng: 'en',
+    interpolation: {
+      escapeValue: false
+    }
+  })
+
+export default i18n
 
