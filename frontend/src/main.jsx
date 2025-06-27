8f1ffb3339f4aa77293dc7aaee1bd23d925b222a
Schrdngr <73908443+Schrdngr@users.noreply.github.com>
2025-06-27 08:03:55 +0300

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,1 +1,14 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
+import './index.css'
+import './i18n'
+
+const root = document.getElementById('root')
+
+ReactDOM.createRoot(root).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+)
 
